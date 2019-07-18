51d2b6e17fde671ac2400843e36b4cac75278561
gsc_prukol <gsc.prukol@gmail.com>
2019-07-18 17:41:20 +0300

    Expanded list of questions to which the bot corresponds. Disabled preview on answers with those who print.

--- main.py
+++ main.py
@@ -23,17 +23,43 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 
+@dp.message_handler(regexp=RE.hello)
+async def hello(message: types.Message):
+    await bot.send_message(message.chat.id, MSG.hello, disable_web_page_preview=True)
+
+
 @dp.message_handler(regexp=RE.printing)
 async def prints(message: types.Message):
     await bot.send_message(message.chat.id, MSG.firstPhrase, reply_to_message_id=message.message_id)
-    await bot.send_message(message.chat.id, MSG.printing) # todo підключити бота до бд
+    await bot.send_message(message.chat.id, MSG.printing, disable_web_page_preview=True)
 
 
 @dp.message_handler(regexp=RE.clinic)
 async def clinic(message: types.Message):
     await bot.send_message(message.chat.id, MSG.firstPhrase, reply_to_message_id=message.message_id)
-    await bot.send_message(message.chat.id, MSG.clinic) # todo підключити бота до бд
+    await bot.send_message(message.chat.id, MSG.clinic)
 
+
+@dp.message_handler(regexp=RE.numberManager)
+async def numberManager(message: types.Message):
+    await bot.send_message(message.chat.id, MSG.firstPhrase, reply_to_message_id=message.message_id)
+
+
+@dp.message_handler(regexp=RE.numberTA)
+async def numberTA(message: types.Message):
+    await bot.send_message(message.chat.id, MSG.firstPhrase, reply_to_message_id=message.message_id)
+
+
+@dp.message_handler(regexp=RE.fullNameCastellansha)
+async def fullNameCastellansha(message: types.Message):
+    await bot.send_message(message.chat.id, MSG.firstPhrase, reply_to_message_id=message.message_id)
+    await bot.send_message(message.chat.id, MSG.fullNameCastellansha)
+
+
+@dp.message_handler(regexp=RE.fullNameManager)
+async def fullNameManager(message: types.Message):
+    await bot.send_message(message.chat.id, MSG.firstPhrase, reply_to_message_id=message.message_id)
+    await bot.send_message(message.chat.id, MSG.fullNameManager)
 
 async def on_startup(dp):
     await bot.set_webhook(WEBHOOK_URL)
@@ -41,7 +67,8 @@ async def on_startup(dp):
 
 async def on_shutdown(dp):
     # insert code here to run it before shutdown
-    await bot.delete_webhook()
+    pass
+
 
 
 if __name__ == '__main__':

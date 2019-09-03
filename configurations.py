d1aaa4551331986df75f5010c645b4c8516194af
gsc_prukol <gsc.prukol@gmail.com>
2019-09-03 21:26:16 +0300

    Edit name castellansha

--- configurations.py
+++ configurations.py
@@ -7,7 +7,7 @@ class Messages:
 4) 4-01 t.me/Froooger, t.me/Drectar, 0,75 грн/А4 (лише чб)"""
     clinic = "Номери реєстратури: (044) 204-95-93 і (044) 204-85-62. Телефонуй їм."
     fullNameManager = 'Олена Вікторівна (в телеграмі підписана як "Елена")'
-    fullNameCastellansha = 'Білоцька Ірина Валентинівна'
+    fullNameCastellansha = 'Зоя Анатоліївна'
     helloReply = 'Hello world!'
     helloNewMembers = 'Привіт) Вітаю в нашому неламповому чатіку'
 

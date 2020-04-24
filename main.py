7c670323c276d40335078797c82e477e0afd2a74
volodymyr_volodko <volodymyr.volodko@evinent.com>
2020-04-24 21:08:47 +0300

    Dev add long pooling

--- main.py
+++ main.py
@@ -78,6 +78,4 @@ async def on_shutdown(dp):
 
 
 if __name__ == '__main__':
-    start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH,
-                  on_startup=on_startup, on_shutdown=on_shutdown,
-                  host=WEBAPP_HOST, port=WEBAPP_PORT)
+    executor.start_polling(dp, skip_updates=True)
